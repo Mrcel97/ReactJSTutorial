29cb77fed894edfa36f71d083bf38a0a8150bf66
Marcel Ortiz Sanchez <9719marcel@gmail.com>
2018-08-21 15:22:37 +0200

    3rd step: Link Board Component with the Game Component.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class Game extends React.Component {
         return (
         <div className="game">              {/* React comment example */}
             <div className="game-board">    {/* Space destined to host an specific component */}
-            Game Board
+                <Board />                   {/* (3rd step) Link Board component with Game component */}
             </div>
             <div className="game-info">     {/* Space destined to log Game actions */}
             </div>
@@ -15,6 +15,29 @@ class Game extends React.Component {
         );
     }
 }
+
+
+class Board extends React.Component {
+    render() {
+        const status = 'Next Player X';     // Board need to know who is playing, set initial Board state to player 'X' or 'O'.
+
+        return (                            // We want a [3row x 3col] tic-tac-toe
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">Row
+                    {/* Insert row-Squares-amount here (4th step) */}
+                </div>
+                <div className="board-row">Row
+                    {/* Insert row-Squares-amount here (4th step) */}
+                </div>
+                <div className="board-row">Row
+                    {/* Insert row-Squares-amount here (4th step) */}
+                </div>
+            </div>
+        );
+    }
+}
+
 
 // ==================================
 
