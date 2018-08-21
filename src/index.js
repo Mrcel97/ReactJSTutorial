fbc869676696dcc0de2ea142b96e9c17439849e1
Marcel Ortiz Sanchez <9719marcel@gmail.com>
2018-08-21 14:01:11 +0200

    1st step: Design Game component.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+class Game extends React.Component {
+    render() {                              // Render html code in js file (BAD PRACTICE).//
+        return (
+        <div className="game">              {/* React comment example */}
+            <div className="game-board">    {/* Space destined to host an specific component */}
+            Game Board
+            </div>
+            <div className="game-info">     {/* Space destined to log Game actions */}
+            </div>
+        </div>
+        );
+    }
+}
