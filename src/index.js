ea82ad7b800294b25c6d0977402a5e2fb292909a
Marcel Ortiz Sanchez <9719marcel@gmail.com>
2018-08-22 11:51:51 +0200

    0th step: Clean 3rd-part comments.

--- src/index.js
+++ src/index.js
@@ -18,24 +18,24 @@ class Game extends React.Component {
 
 
 class Board extends React.Component {
-    constructor(props) {                                    // Add Square Components (children) behaviour to Board Component (parent)
+    constructor(props) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null),                   // 3x3 Array of no-state cells.
+            squares: Array(9).fill(null),
         };
     }
 
     handleClick(i) {
-        const squares = this.state.squares.slice();         // Shallow copy
-        squares[i] = 'X';                                   // (*)... Then this number will be used to update only the specific Square.
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
         this.setState({squares: squares});
     }
 
-    renderSquare(i) {                                       // When we create a Square we assign a numer to it ...(*)
+    renderSquare(i) {
         return (
             <Square 
-                value={this.state.squares[i]}               // Set the 'this.props.value' of the Square Component to 'X', 'O' or Null.
-                onClick={() => this.handleClick(i)}         // Function defined by Board Component to help us to update Board state without privacity problems.
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
             />
         );
     }
@@ -78,23 +78,9 @@ class Square extends React.Component {
         return (
             <button
                 className="square" 
-                onClick={() => this.props.onClick()}        // onClick() will generate a 'click event' handled by handleClick() function in Board Component.
+                onClick={() => this.props.onClick()}
             >
-                {this.props.value}                          {/* Now we are interessed in work with 'this.props.value' due to be updated by the BoardComponent 
-                                                              * 
-                                                              * Explication:
-                                                              *     · State:
-                                                              *         - Individual local states.
-                                                              *         - Often become the props of the Child Components.
-                                                              *         - Inmutable (by any other Component)
-                                                              *         - Mutable (by the same Component)
-                                                              *     
-                                                              *     · Props:
-                                                              *         - External given stats.
-                                                              *         - Always given by a Parent Component.
-                                                              *         - Inmutable (by the same or other components)
-                                                              *         - Mutable (by the Parent Component) <- We want this data flow
-                                                              * */}
+                {this.props.value}
             </button>
         );
     }
